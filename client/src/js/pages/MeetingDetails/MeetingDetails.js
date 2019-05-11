6efc4d4f0df656df902898ac541ed40cd36fb414
PurujitD <PurujitD@gmail.com>
2019-05-11 01:37:50 +0000

    [Christie/Purujit] default meeting date is tomorrow

--- client/src/js/pages/MeetingDetails/MeetingDetails.js
+++ client/src/js/pages/MeetingDetails/MeetingDetails.js
@@ -3,8 +3,13 @@ const StorageHub = require('watch-framework').StorageHub;
 
 class MeetingDetails extends BasePage {
   template = require('./MeetingDetails.hbs');
+
+  pageWillLoad(){
+    this.tomorrowDate = this.getTomorrowsDate(Date.now());
+  }
+
  getTomorrowsDate(todayDate) {
-    var dateTime = new Date(todayDate.getTime() + 1 * 24 * 60* 60 * 1000);
+    var dateTime = new Date(todayDate + 1 * 24 * 60* 60 * 1000);
     return dateTime.toLocaleString('en-AU').split(",")[0];
   }
 
